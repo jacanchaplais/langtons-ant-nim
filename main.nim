f6b30015bc82a7a3a461bd601e21ffa12c430e4b
Jacan Chaplais <jacan.chaplais@gmail.com>
2024-08-28 17:54:22 +0100

    initial commit

---
+++ main.nim
@@ -0,0 +1,75 @@
+import algorithm
+import strutils
+import math
+
+
+const
+  NUM_ROWS = 100
+  NUM_COLS = 100
+
+type
+  Direction = enum
+    UP, RIGHT, DOWN, LEFT
+
+  Ant = object
+    x*: int
+    y*: int
+    alive*: bool = true
+    direction: Direction
+
+  Grid = array[NUM_ROWS, array[NUM_COLS, bool]]
+
+
+proc move(ant: var Ant, grid: var Grid) =
+  let
+    x = ant.x
+    y = ant.y
+  if (x == 0) or (x == NUM_COLS) or (y == 0) or (y == NUM_ROWS):
+    ant.alive = false
+  if not ant.alive:
+    return
+  let cell = grid[y][x]
+  if cell:
+    ant.direction = 
+      if ant.direction != LEFT:
+        ant.direction.succ
+      else:  # cycle to beginning
+        UP
+  else:
+    ant.direction =
+      if ant.direction != UP:
+        ant.direction.pred
+      else:  # go to end
+        LEFT
+  grid[y][x] = not cell
+  case ant.direction:
+    of RIGHT:
+      ant.x += 1
+    of DOWN:
+      ant.y += 1
+    of LEFT:
+      ant.x -= 1
+    of UP:
+      ant.y -= 1
+
+
+proc main() =
+  var out_file = open("grid-data.txt", fmWrite)
+  defer: close(out_file)
+  var ant = Ant(
+    x: floorDiv(NUM_COLS, 2),
+    y: floorDiv(NUM_ROWS, 2),
+    direction: UP
+  )
+  var grid: Grid
+  for row_idx in 0 ..< len(grid):
+    grid[row_idx].fill(false)
+  while ant.alive:
+    move(ant, grid)
+    for row in grid:
+      out_file.write(row.join(","))
+      out_file.write("\n")
+    out_file.write("\n")
+
+
+main()
